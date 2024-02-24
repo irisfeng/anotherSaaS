13cacfc07bc9b47db24355992ea81a2e74a14560
irisfeng <tonifang@189.cn>
2024-02-24 12:11:30 +0000

    mongodb and models added

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,44 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+    title: string; // required
+    transformationType: string; // required
+    publicId: string; // required
+    secureUrl: string; // required, assuming URL can be represented as string
+    width?: number; // optional
+    height?: number; // optional
+    config?: object; // optional, could be further detailed with a specific interface
+    transformationUrl?: string; // optional, assuming URL can be represented as string
+    aspectRatio?: string; // optional
+    color?: string; // optional
+    prompt?: string; // optional
+    author: {
+        _id: String;
+        firstName: String;
+        lastName: String;
+    };
+    createdAt?: Date; // optional, default value provided in schema
+    updatedAt?: Date; // optional, default value provided in schema
+  }
+  
+
+const ImageSchema = new Schema({
+    title: { type: String, required: true },
+    transformationType: { type: String, required: true },
+    publicId: { type: String, required: true },
+    secureUrl: { type: URL, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: URL },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model('Image', ImageSchema);
+
+export default Image;
